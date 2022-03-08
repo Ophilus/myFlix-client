c1c41175d0fffda044525b17584af7c5e6d133db
Vadim Hryn <gryn.vadim@gmail.com>
2022-03-07 23:11:56 -0500

    implemented the client part of registration and login

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,29 +1,61 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Form, Button, Card, Row, Navbar, Nav, Container } from 'react-bootstrap';
+import axios from 'axios';
 
 export function LoginView(props) {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [ usernameErr, setUsernameErr ] = useState('');
+    const [ passwordErr, setPasswordErr ] = useState('');
+  
+
+    const validate = () => {
+      let isReq = true;
+      const regEx = /^[0-9a-zA-Z]+$/;
+      if(!username){
+       setUsernameErr('Username Required');
+       isReq = false;
+      }else if(username.length < 5){
+       setUsernameErr('Username must be 5 characters long');
+       isReq = false;
+      }else if(!username.match(regEx)){
+        setUsernameErr('Username contains non alphanumeric characters - not allowed');
+      }else{
+        setUsernameErr('Ok');
+      }
+      if(!password){
+       setPasswordErr('Password Required');
+       isReq = false;
+      }else if(password.length < 6){
+       setPasswordErr('Password must be 6 characters long');
+       isReq = false;
+      }else{
+        setPasswordErr('Ok');
+      }
+  
+      return isReq;
+  }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(username, password);
-        /* Send a request to the server for authentication */
-        /* then call props.onLoggedIn(username) */
-        props.onLoggedIn(username);
-    };
+        const isReq = validate();
+  if(isReq) {
+        axios.post('https://movieapi-1823.herokuapp.com/login', {
+          Username: username,
+          Password: password
+        })
+        .then(response => {
+          const data = response.data;
+          props.onLoggedIn(data);
+        })
+        .catch(e => {
+          console.log('no such user')
+        });
+      }
+      };
 
     return (
-      <div>
-      <Navbar expand="lg" bg="#808080" className="mainNavbar">
-        <Container>
-        <Navbar.Brand href="/">My Flix</Navbar.Brand>
-            <Nav className="me-auto">
-            <Nav.Link href="#register">Sign up</Nav.Link>
-            </Nav>
-        </Container>
-        </Navbar>
         <Card className="py-4 shadow"> 
          <Row className="justify-content-md-center">
             
@@ -32,11 +64,13 @@ export function LoginView(props) {
       <Form.Group controlId="formUsername">
         <Form.Label>Username:</Form.Label>
         <Form.Control type="text" onChange={e => setUsername(e.target.value)} placeholder="Enter a username" required />
+        {usernameErr && <p>{usernameErr}</p>}
       </Form.Group>
 
       <Form.Group controlId="formPassword">
         <Form.Label>Password:</Form.Label>
         <Form.Control type="password" onChange={e => setPassword(e.target.value)} placeholder="Enter a password" required />
+        {passwordErr && <p>{passwordErr}</p>}
       </Form.Group>
       
       <Button className="m-3" variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
@@ -44,7 +78,6 @@ export function LoginView(props) {
     </Form>
     </Row>
     </Card>
-    </div>
     );
 }
 
--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,53 +1,115 @@
 import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import "./registration-view.scss";
 import { Form, Button, Card, Row, Navbar, Nav, Container } from 'react-bootstrap';
+import axios from 'axios';
 
 export function RegistrationView(props) {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [birthday, setBirthday] = useState('');
+    const [ usernameErr, setUsernameErr ] = useState('');
+    const [ passwordErr, setPasswordErr ] = useState('');
+    const [ emailErr, setEmailErr ] = useState('');
+
+    const validate = () => {
+      let isReq = true;
+      const regEx = /^[0-9a-zA-Z]+$/;
+      if(!username){
+       setUsernameErr('Username Required');
+       isReq = false;
+      }else if(username.length < 5){
+       setUsernameErr('Username must be 5 characters long');
+       isReq = false;
+      }else if(!username.match(regEx)){
+        setUsernameErr('Username contains non alphanumeric characters - not allowed');
+      }else{
+        setUsernameErr('Ok');
+      }
+      if(!password){
+       setPasswordErr('Password Required');
+       isReq = false;
+      }else if(password.length < 6){
+       setPasswordErr('Password must be 6 characters long');
+       isReq = false;
+      }else{
+        setPasswordErr('Ok');
+      }
+      if(!email){
+        setEmailErr('Email Required');
+        isReq = false;
+       }else if(email.indexOf('@') === -1){
+        setEmailErr('Email is not suitable. I do not see "@"');
+        isReq = false;
+       }else{
+         setEmailErr('Ok');
+       }
+  
+      return isReq;
+  }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(username, password);
+        
         props.onLoggedIn(username);
     };
-
+    const handleRegister = (e) => {
+      e.preventDefault();
+      const isReq = validate();
+  if(isReq) {
+      axios.post('https://movieapi-1823.herokuapp.com/users', {
+        Username: username,
+        Password: password,
+        Email: email,
+        Birthday: birthday
+      })
+      .then(response => {
+        const data = response.data;
+        console.log(data);
+        window.open('/', '_self'); // the second argument '_self' is necessary so that the page will open in the current tab
+      })
+      .catch(e => {
+        console.log('error registering the user')
+      });
+    }
+  };
+    
     return (
-        <div>
-      <Navbar expand="lg" bg="#808080" className="mainNavbar">
-        <Container>
-        <Navbar.Brand href="/">My Flix</Navbar.Brand>
-            <Nav className="me-auto">
-            <Nav.Link href="#login">Log in</Nav.Link>
-            </Nav>
-        </Container>
-        </Navbar>
         <Card className="py-4 shadow"> 
         <Row className="justify-content-md-center">
            
        
        <Form >
-       <Form.Group controlId="formEmail">
-       <Form.Label>Email:</Form.Label>
-       <Form.Control type="email" onChange={e => setEmail(e.target.value)} placeholder="Enter an email"/>
-     </Form.Group>
+       
      <Form.Group controlId="formUsername">
        <Form.Label>Username:</Form.Label>
        <Form.Control type="text" onChange={e => setUsername(e.target.value)} placeholder="Enter a username" />
+       {usernameErr && <p>{usernameErr}</p>}
      </Form.Group>
 
      <Form.Group controlId="formPassword">
        <Form.Label>Password:</Form.Label>
        <Form.Control type="password" onChange={e => setPassword(e.target.value)} placeholder="Enter a password" />
+       {passwordErr && <p>{passwordErr}</p>}
      </Form.Group>
-     <Button className="m-3" variant="primary" type="submit" onClick={handleSubmit}>
+     <Form.Group controlId="formEmail">
+       <Form.Label>Email:</Form.Label>
+       <Form.Control type="email" onChange={e => setEmail(e.target.value)} placeholder="Enter an email"/>
+       {emailErr && <p>{emailErr}</p>}
+     </Form.Group>
+     <Form.Group controlId="formDate">
+       <Form.Label>Date of birth:</Form.Label>
+       <Form.Control type="date" onChange={e => setBirthday(e.target.value)} placeholder="Enter your date of birth"/>
+     </Form.Group>
+     <Button className="m-3" variant="primary" type="submit" onClick={handleRegister}>
        Submit
      </Button>
    </Form>
    </Row>
    </Card>
-   </div>
+  
     );
 }
 
@@ -55,6 +117,7 @@ RegistrationView.propTypes = {
     register: PropTypes.shape({
       Email: PropTypes.string.isRequired,
       Username: PropTypes.string.isRequired,
-      Password: PropTypes.string.isRequired
+      Password: PropTypes.string.isRequired,
+      Date: PropTypes.string.isRequired
     })
   };
